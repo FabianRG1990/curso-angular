ffc4cd9c0c52d5f9c6bfc67ee2b26ed4205f138b
Fabian Rodriguez <fabian.soy1990@gmail.com>
2022-10-16 18:13:52 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ContadorModule } from './contador/contador.module';
+
+import { HeroesModulo } from './heroes/heroes.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HeroesModulo, ContadorModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { contadorComponent } from './contador.component';
+
+@NgModule({
+  declarations: [contadorComponent],
+  exports: [contadorComponent],
+  imports: [CommonModule],
+})
+export class ContadorModule {}
